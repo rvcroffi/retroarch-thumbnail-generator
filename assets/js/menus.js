cef490eb79b16b6cacf5ae377ec5e25437df0451
Rafael V. Croffi <rvcroffi@msn.com>
2020-05-29 01:49:54 -0300

    about license

--- assets/js/menus.js
+++ assets/js/menus.js
@@ -3,7 +3,7 @@ const { Menu, shell } = require('electron').remote;
 function getMenus(sharedObject) {
   let infoMenu = Menu.buildFromTemplate([
     {
-      label: 'Tutorial',
+      label: 'How it works',
       click: () => {
         // shell.openExternal('https://github.com');
       }
