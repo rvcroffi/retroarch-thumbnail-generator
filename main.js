5f9c905e14aaf245e8ded5d9f293a1ab8f8f842f
--global <rvcroffi@msn.com>
2020-06-01 01:44:56 -0300

    update icons

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ function createWindow() {
     webPreferences: {
       preload: path.join(app.getAppPath(), 'assets', 'js', 'preload.js')
     },
-    icon: __dirname + '/build/icons/32x32.png'
+    icon: __dirname + '/build/icon.png'
   });
 
   Menu.setApplicationMenu(null);
